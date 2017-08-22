2a3c93c91509c0f4f8762219e4db543a5399770a
Robby Bourne <robbybourne1@gmail.com>
2017-08-22 10:27:22 -0400

    Fixed while loop

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace dictionary_dotnet
 {
@@ -6,24 +7,33 @@ namespace dictionary_dotnet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please Enter a Name:");
-            var name = Console.ReadLine();
-                while (name != string.Empty)
-                {
-                    Console.WriteLine($"Please Enter a Phone Number assciated with {name}: ");
-                    var phoneNumber = Console.ReadLine();
-                    if (phoneNumber == string.Empty)
-                    {
-                        Console.WriteLine($"Please enter the Phone number assciated with {name}: ");
-                        phoneNumber = Console.ReadLine();
-                    }
-                    else
-                    {
-                        Console.WriteLine($"{name} and {phoneNumber}");
-                        Console.WriteLine("Please Enter a Name:");
-                        name = Console.ReadLine();
-                    }
-                }
+            var myDict = new Dictionary<string, int>();
+            string name = "";
+            string phoneNumberString = "";
+            int phoneNumber;
+            
+            while (name == string.Empty)
+            {
+                Console.WriteLine("Please Enter a Name:");
+                name = Console.ReadLine();
             }
+
+            while (!Int32.TryParse(phoneNumberString, out phoneNumber))
+            {
+                Console.WriteLine($"Please Enter a Phone Number assciated with {name}: ");
+                phoneNumberString = Console.ReadLine();
+            }
+
+            myDict.Add(name, phoneNumber);
+                //             
+                // var 
+                // int phoneNumber;
+                // if ()
+                // {
+                //     Console.WriteLine($"Please enter the Phone number assciated with {name}: ");
+                //     phoneNumber = int.Parse(Console.ReadLine());
+                // ;
+                // }
         }
     }
+}
